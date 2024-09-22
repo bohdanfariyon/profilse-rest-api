e02d3b85eee23130c5633d91aa8d90e46370a4cd
Bohdan <bohdan5621@gmail.com>
2024-09-22 22:16:29 +0300

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from rest_framework import status
-from profiles_api import serializers
 from rest_framework import viewsets
-from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
-from profiles_api import permissions
 from rest_framework import filters
+
+from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
+
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -107,3 +111,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
